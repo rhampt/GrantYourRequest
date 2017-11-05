232001f88d48d2d0ed8b92c8adcb0f3c666d520c
Ryan Hampton <rhampt@umich.edu>
2017-11-05 14:46:04 -0500

    Removed form contact and just added in email address

--- eb-flask/application.py
+++ eb-flask/application.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-from flask.ext.mail import Message, Mail
 import os
 
 # EB looks for an 'application' callable by default.
@@ -8,13 +7,6 @@ project_root = os.path.dirname(__file__)
 template_path = os.path.join(project_root, 'Templates')
 application = Flask(__name__, template_folder=template_path, static_url_path="", static_folder="Static")
 
-
-application.config["MAIL_SERVER"] = "smtp.gmail.com"
-application.config["MAIL_PORT"] = 465
-application.config["MAIL_USE_SSL"] = True
-application.config["MAIL_USERNAME"] = 'grantyourreq@gmail.com'
-application.config["MAIL_PASSWORD"] = 'Oops'
-
 mail=Mail(application)
 
 # use decorators to link the function to a url
@@ -22,17 +14,6 @@ mail=Mail(application)
 def home():
 	if request.method == 'GET':
 		return render_template('index.html')
-	if request.method == 'POST':
-		msg = Message("Message from website(person = " + str(request.form["name"]) +")", sender='grantyourreq@gmail.com', recipients=['grantyourreq@gmail.com'])
-		msg.body = """
-		From: %s
-		Phone Number: %s
-		Email: %s
-		Message:
-		%s
-		""" % (str(request.form["name"]), str(request.form["phone"]), str(request.form["email"]), str(request.form["message"]))
-		mail.send(msg)
-		return 'Form posted.'
 	
 @application.errorhandler(404)
 def page_not_found(e):
