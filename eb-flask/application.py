93632fb7993d79ade03954188fa4b946201c25fb
Ryan Hampton <rhampt@umich.edu>
2017-11-05 14:01:43 -0500

    Totally Fixed stupid Internal server issue *Every time I tried to render_template(‘index.html’) it would return a 500 internal server error *Added the template location into the application object in flask to fix it *Also removed a bunch of unnecessary requirements from requirements.txt

--- eb-flask/application.py
+++ eb-flask/application.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request
 from flask.ext.mail import Message, Mail
+import os
 
 # EB looks for an 'application' callable by default.
-application = Flask(__name__,static_url_path="", static_folder="static")
+# EB looks for an 'application' callable by default.
+project_root = os.path.dirname(__file__)
+template_path = os.path.join(project_root, 'Templates')
+application = Flask(__name__, template_folder=template_path, static_url_path="", static_folder="Static")
+
 
 application.config["MAIL_SERVER"] = "smtp.gmail.com"
 application.config["MAIL_PORT"] = 465
